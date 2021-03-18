942d01e27eddeae95ead98f254e7c821fe3edae3
Grace2614 <80277169+Grace2614@users.noreply.github.com>
2021-03-18 12:02:59 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 //https://www.eclipse.org/paho/clients/js/
 
 function LED1_On() {
-	alert("led on");
+	//alert("led on");
 	console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
 	message = new Paho.MQTT.Message("ON");
@@ -9,7 +9,7 @@ function LED1_On() {
 	client.send(message);
 }
 function LED1_Off(){	
-	alert("led off");
+	//alert("led off");
 	console.log("led off");
 	//document.getElementById("sensor").innerHTML="led off";
 	message = new Paho.MQTT.Message("OFF");
